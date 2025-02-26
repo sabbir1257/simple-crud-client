1174d22d91662b26950839725a03c36525979b9a
sabbir1257 <masabbir1257@gmail.com>
2025-02-26 12:53:52 +0600

    55-3 Create client side post and Insert single user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
 
 function App() {
+  const handleAddUsers = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5001/users", {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Simple Crud Client</h1>
+      <form onSubmit={handleAddUsers}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
