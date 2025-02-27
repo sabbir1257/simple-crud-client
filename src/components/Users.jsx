f2a6fca4815ea3a9a543beba9dfc2e507a739fda
sabbir1257 <masabbir1257@gmail.com>
2025-02-27 10:43:33 +0600

    Delete A User From the Database using UI click button

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,20 +1,37 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData()
+  const users = useLoaderData();
 
-    return (
-        <div>
-            <h2>{users.length}</h2>
-            <div>
-                {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>  )
-                }
-            </div>
-        </div>
-    );
+  const handleDelete = (_id) => {
+    console.log(_id);
+    fetch(`http://localhost:5001/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert('deleted successfully')
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h2>{users.length}</h2>
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} : {user.email} {user._id}
+            <button onClick={() => handleDelete(user._id)}>Delete</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
